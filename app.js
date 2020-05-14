fcb0482c427b408ff8603acdf76aa16743e913dc
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-14 09:16:09 +0200

    code update

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ function exec() {
   const b = parseFloat(prompt('Inserisci b..', 0));
   const c = parseFloat(prompt('Inserisci c..', 0));
 
-  if (a == 0) {
-    document.getElementById('solution').innerHTML =
-      "L'equazione e' di primo grado!";
-  } else {
-    const delta = (b * b) - 4 * a * c;
-    
-    if (delta < 0) {
-      document.getElementById('solution').innerHTML = 'Equazione impossibile';
-    } else if (delta == 0) {
-      document.getElementById('solution').innerHTML = `Risultato: ${-b /
-        (2 * a).toFixed(2)}`;
+  if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    if (a == 0) {
+      document.getElementById('solution').innerHTML =
+        "L'equazione e' di primo grado!";
     } else {
-      document.getElementById('solution').innerHTML = `Soluzioni: ${((-b + Math.sqrt(delta)) / (2 * a)).toFixed(2)}, ${((-b - Math.sqrt(delta)) / (2 * a)).toFixed(2)}`;
+      const delta = b * b - 4 * a * c;
+
+      if (delta < 0) {
+        document.getElementById('solution').innerHTML = 'Equazione impossibile';
+      } else if (delta == 0) {
+        document.getElementById('solution').innerHTML = `Risultato: ${-b /
+          (2 * a).toFixed(2)}`;
+      } else {
+        document.getElementById('solution').innerHTML = `Soluzioni: ${(
+          (-b + Math.sqrt(delta)) /
+          (2 * a)
+        ).toFixed(2)}, ${((-b - Math.sqrt(delta)) / (2 * a)).toFixed(2)}`;
+      }
     }
+  } else {
+    document.getElementById('solution').innerHTML = 'invalid input';
   }
 }
